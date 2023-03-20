173063f73d267c4c58a46677fabdb172fcac52e5
TheFred94 <frederikrlarsen@gmail.com>
2023-03-20 22:33:25 +0100

    collapsibles work and when one is open and a new opens the first closes. Still takes up the whole screen

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 "use strict";
+
+let coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    // Close any open collapsibles
+    let openCollapsible = document.querySelector(".collapsible.active");
+    if (openCollapsible && openCollapsible !== this) {
+      openCollapsible.classList.remove("active");
+      openCollapsible.nextElementSibling.style.maxHeight = null;
+    }
+
+    // Toggle the clicked collapsible
+    this.classList.toggle("active");
+    let content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
